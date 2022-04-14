d41fe96e16e0c70dd1b51c130d6125c26374bb0d
Ashutosh2021 <76943231+Ashutosh2021@users.noreply.github.com>
2022-04-14 23:14:15 +0530

    Add files via upload

---
+++ data.py
@@ -0,0 +1,67 @@
+data=[
+        {'distance': '0.000015813',
+        'gravity': '274.2814900104337',
+        'mass': '1.989e+30',
+        'name': 'Sun',
+        'radius': '695700000.0'},
+
+        {'distance': '0008.6',
+        'gravity': '196.980653541914',
+        'mass': '4.1769e+30',
+        'name': 'Sirius',
+        'radius': '1189647000.0'},
+
+        {'distance': '0004.4',
+        'gravity': '209.52058264685908',
+        'mass': '2.1879e+30',
+        'name': 'Alpha Centauri',
+        'radius': '834840000.0'},
+
+        {'distance': '0017',
+        'gravity': '152.37860556135206',
+        'mass': '3.5802e+30',
+        'name': 'Altair',
+        'radius': '1252260000.0'},
+
+        {'distance': '0025',
+        'gravity': '160.84408364809383',
+        'mass': '3.7790999999999997e+30',
+        'name': 'Fomalhaut',
+        'radius': '1252260000.0'},
+
+        {'distance': '011.9',
+        'gravity': '269.3117762743067',
+        'mass': '1.557387e+30',
+        'name': 'Tau Ceti',
+        'radius': '621260100.0'},
+        
+        {'distance': '019.92',
+        'gravity': '182.6209060738644',
+        'mass': '1.9710989999999998e+30',
+        'name': 'Delta Pavonis',
+        'radius': '848754000.0'},
+        
+        {'distance': '044.25',
+        'gravity': '159.0291692445447',
+        'mass': '2.52603e+30',
+        'name': 'Titawin',
+        'radius': '1029636000.0'},
+        
+        {'distance': '036.1',
+        'gravity': '235.914364427263',
+        'mass': '1.51164e+30',
+        'name': '54 Piscium',
+        'radius': '653958000.0'},
+        
+        {'distance': '35.9',
+        'gravity': '233.2325595326817',
+        'mass': '2.9835e+31',
+        'name': 'WISE 0458+6434A',
+        'radius': '2921940000.0'},
+        
+        {'distance': '35.9',
+        'gravity': '189.94563020113137',
+        'mass': '1.989e+31',
+        'name': 'WISE 0458+6434B',
+        'radius': '2643660000.0'}
+    ]
